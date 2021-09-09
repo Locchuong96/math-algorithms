dcce09532ed45ec053af3116c088ca7d2971da4e
Loc Chuong <73679364+Locchuong96@users.noreply.github.com>
2021-09-10 05:21:53 +0700

    Add files via upload

---
+++ Orientation Detection/cop5/BoardDisplay_EKF.py
@@ -0,0 +1,153 @@
+import Wireframe_EKF as wf
+import readSensor_tcp as tcp
+import pygame  
+from operator import itemgetter 
+
+def initializeBlock():
+	block = wf.Wireframe()
+	block_nodes = [(x,y,z) for x in (-1.5,1.5) for y in (-1,1) for z in (-0.1,0.1)] #list of (x,y,z)
+	node_colors = [(255,255,255)] * len(block_nodes)
+	block.addNodes(block_nodes,node_colors)
+	block.outputNodes() #print out infomation of block
+	faces = [(0, 2, 6, 4), (0, 1, 3, 2), (1, 3, 7, 5), (4, 5, 7, 6), (2, 3, 7, 6), (0, 1, 5, 4)]
+	colors = [(255, 0, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255), (255, 255, 0)]
+	block.addFaces(faces,colors)
+	block.outputFaces()
+
+	return block
+
+class ProjectionViewer:
+	def __init__(self,width,height,wireframe):
+		""" Display 3D objects on Pygame screen """
+		self.width = width
+		self.height = height 
+		self.wireframe = wireframe #the block created by initializeBlock function
+		self.screen = pygame.display.set_mode((width,height))
+		pygame.display.set_caption("Attitude Determination using Quaternions")
+		self.background = (10,10,50)
+		self.clock = pygame.time.Clock()
+		pygame.font.init()
+		self.font = pygame.font.SysFont("Comic Sans MS",20)
+
+	def run(self,sensorInstance):
+		""" Create a pygame screen until it is closed """
+		running = True 
+		loopRate = 50
+		# angularVelocity = [0.25,0.25,0.25] # set the gyro from sensor
+		while running:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					running = False
+					sensorInstance.close() # close the connection sensor
+
+			self.clock.tick(loopRate) # tick the internal block
+			angularVelocity = [sensorInstance.data[3],sensorInstance.data[4],sensorInstance.data[5]] # get the gyro from sensor
+			self.wireframe.quatRotate(angularVelocity, 1/loopRate) # dt = 1/loopRate
+			self.display()
+			pygame.display.flip() # update the screen python
+
+	# One vanishing point perspective view algorithm NOT USE
+	def projectOnePointPerspective(self,x,y,z,win_width,win_height,P,S,scaling_constant,pvDepth):
+		# In Pygame, the y axis is downward pointing
+		# In order to make y point upwards, a rotation around x axis by 180 degrees is needed
+		# This will result in y' = -y, z' = -z
+		xPrime = x 
+		yPrime = -y
+		zPrime = -z
+		xProjected = XPrime * (S/ (zPrime + P)) * scaling_constant + win_width/2  # move your object into center of screen
+		yProjected = YPrime * (S/ (zPrime + P)) * scaling_constant + win_height/2 # move your object into center of screen
+		pvDepth.append(1/(zPrime + P))
+		return (round(xProjected),round(yProjected))
+
+	#Normal projection
+	def projectOthorgraphic(self,x,y,z,win_width,win_height,scaling_constant,pvDepth):
+		# In Pygame, the y axis is downward pointing
+		# In order to make y point upwards, a rotation around x axis by 180 degrees is needed
+		# This will result in y' = -y, z' = -z
+		xPrime = x 
+		yPrime = -y
+		xProjected = xPrime * scaling_constant + win_width /2
+		yProjected = yPrime * scaling_constant + win_height /2
+		# Note that there is no negative sign here because our rotation to computer frame
+		# assumes that the computer frame is x-right, y-up, z-out
+		# so this rotation z-coordinate below is already in the outward direction
+		pvDepth.append(z)
+		return (round(xProjected),round(yProjected))
+
+	def messageDisplay(self,text,x,y,color):
+		#display the message of angle on the screen
+		textSurface = self.font.render(text,True,color,self.background)
+		textRect = textSurface.get_rect()
+		textRect.topleft = (x,y)
+		self.screen.blit(textSurface,textRect) # draw a textRect with textSurface on the screen
+
+	def display(self):
+		""" Draw wirefames on the screen. """
+		self.screen.fill(self.background)
+
+		#Get the current attitude
+		roll, pitch, yaw = self.wireframe.getAttitude() # return the roll, pitch,yaw 
+
+		self.messageDisplay("Roll %.1f" % roll,self.screen.get_width()*0.75,self.screen.get_height()*0,(220,20,60))
+		self.messageDisplay("Pitch %.1f" % pitch,self.screen.get_width()*0.75,self.screen.get_height()*0.05,(0,255,255))
+		self.messageDisplay("Yaw %.1f" % yaw,self.screen.get_width()*0.75,self.screen.get_height()*0.1,(65,105,255))
+
+		# Transform nodes o perspective view
+		dist = 5
+		pvNodes = []
+		pvDepth = []
+		# loop over the nodes in block, caculate itself othographic value and storage in pvNodes
+		for node in self.wireframe.nodes:
+			
+			point = [node.x,node.y,node.z] # create a point 
+			newCoord = self.wireframe.rotatePoint(point) #rotate this point
+			comFrameCoord = self.wireframe.convertToComputerFrame(newCoord)
+
+			pvNodes.append(self.projectOthorgraphic(comFrameCoord[0],comFrameCoord[1],comFrameCoord[2],
+								self.screen.get_width(),self.screen.get_height(),70,pvDepth))
+
+			""" NOT USE
+			pvNodes.append(self.projectOnePointPerspective(node.x,node.y,node.z,self.screen.get_width(),self.screen.get_height(),5,10,30,pvDepth))
+			"""
+			# storage the z value of node to pvDepth
+			#pvDepth.append(node.z)
+
+		#print("pvDepth",pvDepth)
+		
+		# Calculate the average Z values of each face 
+		avg_z = []
+		#loop over each face in the the block
+		for face in self.wireframe.faces:
+			n = pvDepth 
+			# calculate the average z value of each face using nodeIndexes ex (2,6,4,0)
+			z = (n[face.nodeIndexes[0]] + n[face.nodeIndexes[1]] + n[face.nodeIndexes[2]] + n[face.nodeIndexes[3]]) / 4.0 
+			avg_z.append(z) 
+
+		#print("avg_z",avg_z)
+
+		# Draw the faces using the Painter's algorithm:
+		for idx, val in sorted(enumerate(avg_z),key = itemgetter(1)):
+			face = self.wireframe.faces[idx]
+			pointList = [pvNodes[face.nodeIndexes[0]],
+						pvNodes[face.nodeIndexes[1]],
+						pvNodes[face.nodeIndexes[2]],
+						pvNodes[face.nodeIndexes[3]]]
+
+			#print(pointList)
+
+			pygame.draw.polygon(self.screen,face.color,pointList)
+
+if __name__ == "__main__":
+
+	numParams = 9
+	url = "http://192.168.1.11"
+	timeout = 1
+	s =  tcp.TcpRead(url = url,numParams = numParams,timeout = timeout)
+	s.readtcpStart()
+
+	block = initializeBlock()
+	pv = ProjectionViewer(640,480,block)
+	pv.run(s)
+
+
+
---
+++ Orientation Detection/cop5/Wireframe_EKF.py
@@ -0,0 +1,61 @@
+import numpy as np 
+import Quaternion_simple as quat 
+#Node stores each point of block
+class Node:
+	def __init__(self,coordinates,color):
+		self.x = coordinates[0]
+		self.y = coordinates[1]
+		self.z = coordinates[2]
+		self.color = color
+
+#Face stores 4 nodes that make up a face of the block
+class Face:
+	def __init__(self,nodes,color):
+		self.nodeIndexes = nodes
+		self.color = color
+
+# Wireframe stores the details of the block
+class Wireframe:
+	def __init__(self):
+		self.nodes = []
+		self.edges = []
+		self.faces = []
+		self.quaternion = quat.Quaternion() #create a quaternion
+
+	def addNodes(self,nodeList,colorList):
+		for node,color in zip(nodeList,colorList):
+			self.nodes.append(Node(node,color))
+
+	def addFaces(self,faceList,colorList):
+		for indexes,color in zip(faceList,colorList):
+			self.faces.append(Face(indexes,color))
+
+	def outputNodes(self):
+		print("\n---Nodes---")
+		for i,node in enumerate(self.nodes):
+			print("id: %d Position: (%.2f,%2.f,%2.f) \t Color: (%d, %d, %d)" % 
+					(i,node.x,node.y,node.z,node.color[0],node.color[1],node.color[2]))
+
+	def outputFaces(self):
+		print("\n---Faces---")
+		for i,face in enumerate(self.faces):
+			print("id: %d: \t Color (%.2f,%.2f,%.2f)" % (i,face.color[0],face.color[1],face.color[2]))
+			for nodeIndex in face.nodeIndexes:
+				print("\t Node %d" % nodeIndex)
+
+	#Add new function for quaternion
+	
+	def quatRotate(self,w,dt):
+		self.quaternion.rotate(w,dt)
+
+	def rotatePoint(self,point):
+		rotationMat = quat.getRotMat(self.quaternion.q)
+		return np.matmul(rotationMat,point)
+
+	def convertToComputerFrame(self,point):
+		computerFrameChangeMatrix = np.array([[-1,0,0],[0,0,-1],[0,-1,0]])
+		return np.matmul(computerFrameChangeMatrix,point)
+
+	def getAttitude(self):
+		return quat.getEulerAngles(self.quaternion.q)
+
